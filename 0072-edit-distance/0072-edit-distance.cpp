eefc0b003e9154ca7d0fc3bd6dd3a1c62a538244
Shivansh Yadav <shivanshyadav50@gmail.com>
2023-10-25 23:25:54 +0530

    Time: 15 ms (38.27%), Space: 9.5 MB (22.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,6 +3,7 @@ class Solution {
   int minDistance(string word1, string word2) {
     const int m = word1.length();
     const int n = word2.length();
+    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
     for (int i = 1; i <= m; ++i)
@@ -17,7 +18,7 @@ class Solution {
           dp[i][j] = dp[i - 1][j - 1];
         else
           dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             
+
     return dp[m][n];
   }
 };

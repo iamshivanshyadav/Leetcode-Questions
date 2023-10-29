dc86315f204d99bb434e0be1c72edf49ddaa9ebd
Shivansh Yadav <shivanshyadav50@gmail.com>
2023-10-29 19:14:48 +0530

    Time: 9 ms (15.12%), Space: 14.4 MB (29.71%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,40 +1,29 @@
-
 class Solution {
-public:
-    bool search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        
-        while(l <= r)
-        {
-            int mid = l + (r-l) / 2;
-            if (nums[mid] == target)
-                return true;
+ public:
+  bool search(vector<int>& nums, int target) {
+    int l = 0;
+    int r = nums.size() - 1;
 
-            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
-            {
-                l++;
-                r--;
-            }
-
-            else if(nums[l] <= nums[mid])
-            {
-
-                if((nums[l] <= target) && (nums[mid] > target))
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-
-            else
-            {
-                if((nums[mid] < target) && (nums[r]>= target))
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
-        }
-        return false;
+    while (l <= r) {
+      const int m = (l + r) / 2;
+      if (nums[m] == target)
+        return true;
+      if (nums[l] == nums[m] && nums[m] == nums[r]) {
+        ++l;
+        --r;
+      } else if (nums[l] <= nums[m]) {  
+        if (nums[l] <= target && target < nums[m])
+          r = m - 1;
+        else
+          l = m + 1;
+      } else {  
+        if (nums[m] < target && target <= nums[r])
+          l = m + 1;
+        else
+          r = m - 1;
+      }
     }
 
+    return false;
+  }
 };

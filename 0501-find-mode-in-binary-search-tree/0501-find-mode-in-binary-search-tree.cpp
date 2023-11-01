21f13d88bb32e4f0fddb6c3f37e58d400ba410bd
Shivansh Yadav <shivanshyadav50@gmail.com>
2023-11-01 16:02:24 +0530

    Time: 17 ms (52.27%), Space: 24.3 MB (82.92%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    vector<int> findMode(TreeNode* root) {
-        vector<int> inorderr;
-        inorder(root, inorderr);
-        unordered_map<int, int> freq;
-        int maxx = 0;
-        
-        for (int val : inorderr) {
-            freq[val]++;
-            maxx = max(maxx, freq[val]);
-        }
-        
-        vector<int> result;
-        for (const auto& pair : freq) {
-            if (pair.second == maxx) {
-                result.push_back(pair.first);
-            }
-        }
-        
-        return result;
+    std::vector<int> findMode(TreeNode* root) {
+        in_order_traversal(root);
+        return modes;
     }
-
 private:
-    void inorder(TreeNode* root, vector<int>& result) {
-        if (!root) return;
-        result.push_back(root->val);
-        inorder(root->left, result);
-        inorder(root->right, result);
+    int current_val = 0;
+    int current_count = 0;
+    int max_count = 0;
+    std::vector<int> modes;
+
+    void in_order_traversal(TreeNode* node) {
+        if (!node) return;
+        
+        in_order_traversal(node->left);
+        
+        current_count = (node->val == current_val) ? current_count + 1 : 1;
+        if (current_count == max_count) {
+            modes.push_back(node->val);
+        } else if (current_count > max_count) {
+            max_count = current_count;
+            modes = { node->val };
+        }
+        current_val = node->val;
+
+        in_order_traversal(node->right);
     }
 };

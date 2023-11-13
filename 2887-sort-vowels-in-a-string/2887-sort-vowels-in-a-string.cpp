830bb62e92f1f177358ec506fb463f2fdc131d2e
Shivansh Yadav <shivanshyadav50@gmail.com>
2023-11-13 21:32:53 +0530

    Time: 31 ms (91.29%), Space: 16.1 MB (21.73%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,25 +1,42 @@
+#pragma GCC optimize("O3")
 class Solution {
 public:
+    const string vowels="AEIOUaeiou";
+    int freq[10]={0};
+    vector<int> pos;
+    void isVowel(char c, int idx){
+        int i=vowels.find(c);
+        if (i!=-1){
+            freq[i]++;
+            pos.push_back(idx);
+        }
+    }
+    void print(auto & c){
+        for(int i: c) cout<<i<<", ";
+        cout<<endl;
+    }
+
     string sortVowels(string s) {
-
-        vector<char> vowels;
-        for (char c : s) {
-            if (string("aeiouAEIOU").find(c) != string::npos) {
-                vowels.push_back(c);
-            }
+        int n=s.size();
+        #pragma unroll
+        for(int i=0; i<n; i++)
+            isVowel(s[i], i);
+    //    print(pos);
+        int count=0, v=freq[0], j=0;
+        #pragma unroll
+        for (int i: pos){
+            while(count>=v)
+                v+=freq[++j];
+            s[i]=vowels[j];
+            count++;
         }
-        sort(vowels.begin(), vowels.end(), greater<char>());
-
-        string result;
-        for (char c : s) {
-            if (string("aeiouAEIOU").find(c) != string::npos) {
-                result += vowels.back();
-                vowels.pop_back();
-            } else {
-                result += c;
-            }
-        }
-
-        return result;        
+        return s;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
